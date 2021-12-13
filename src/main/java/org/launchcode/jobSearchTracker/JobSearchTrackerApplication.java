b0cfb11f09f9cadd710772edd644c9512d5b59b8
May Lin <40640630+maymlin@users.noreply.github.com>
2021-12-12 19:15:31 -0600

    Add Spring Boot project

---
+++ src/main/java/org/launchcode/jobSearchTracker/JobSearchTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.jobSearchTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSearchTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/jobSearchTracker/JobSearchTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.jobSearchTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
